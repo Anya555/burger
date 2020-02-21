e4ee26fecd138b88b80d797298dae06a608c8990
Anna Panas <annapanas0906@gmail.com>
2020-02-20 19:15:07 -0500

    added functionality to display not devoured burgers

--- public/assets/js/cheeseburger.js
+++ public/assets/js/cheeseburger.js
@@ -2,27 +2,57 @@ $(document).ready(function () {
 
     // selecting input area where user writes burger name
     const burgername = $(".burger-name");
- 
+    let newburger;
+   
+
     $(".submit-btn").on("click", function (e) {
         e.preventDefault();
-
-        let newburger = {
+        newburger = {
             burger_name: burgername.val()
         }
-        console.log(newburger);
         addBurgerToDb(newburger);
+        console.log(newburger);
+     
     });
 
-    
+
+
+
     function addBurgerToDb(Burger) {
-        $.post("/api/burgers/", Burger, function() {
-    // Location.reload() method reloads the current URL, like the Refresh button
+        $.post("/api/burgers/", Burger).then(function () {
+            location.reload();
+            /*
+             console.log(newburger);
+
+            var addToList = [];
+
+            for (let i = 0; i < newburger.length; i++) {
+                    addToList.push(newburger[i]);
+            }
+            notDevoured.append(addToList);
+            console.log(addToList);
+            */
+
+            // location.reload();
+        })
+    }
+
+
+
+
+    $(".devour").on("click", function (e) {
+        e.preventDefault();
+        // e.stopImmediatePropagation();
+
+        // Get the burger id 
+        const id = $(this).attr("data-id");
+        console.log(id);
+
+        // Make api call
+        const url = "/api/burger/" + id;
+        $.put(url).then(function () {
             location.reload();
         });
-      }
-
-    $(".devour").on("click", function () {
-
     });
 
 });
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,10 +2,7 @@
 const db = require("../models");
 
 module.exports = function (app) {
-  // rendering home page
-  app.get('/', function (req, res) {
-    res.render('index');
-  });
+
 
   // adding a new burger to database
   app.post("/api/burgers", function (req, res) {
@@ -18,5 +15,46 @@ module.exports = function (app) {
     });
   });
 
-}
+  app.put("/api/burger/:id", function(req, res){
 
+  });
+
+  // rendering home page
+  app.get('/', function (req, res) {
+    // Use sequelize model to find all burgers
+    // Create two arrays, one for devoured burgers, one for uneaten ones
+    // Send both arrays to handlebars and let it populate the page
+    // console.log("home page");
+
+    
+    const devoured = [];
+    const uneaten = [];
+
+    db.Burgers.findAll({}).then((resp) => {
+      console.log(resp);
+      for (let i = 0; i < resp.length; i++) {
+
+        // When grabbing an object from a database, handlebars won't let you use the native 
+        // object, you have to build a new object and populate it w/ the data handlebaars needs
+        const burger = {
+          id: resp[i].id,
+          burger_name: resp[i].burger_name,
+          devoured: resp[i].devoured
+        }
+
+        if (!burger.devoured) {
+          uneaten.push(burger);
+        } else {
+          devoured.push(burger);
+        }
+      }
+
+      res.render('index', { burgers: { devoured: devoured, uneaten: uneaten }})
+    });
+  });
+
+
+
+
+
+}
